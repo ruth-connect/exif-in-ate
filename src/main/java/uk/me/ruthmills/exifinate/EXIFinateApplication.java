7e2316ef631225be6727f56a6022b45ed232fa9a
ruth <ruth@DESKTOP-HOVL7PT>
2023-07-07 18:00:34 +0100

    Initial commit of Spring Boot stuff.

---
+++ src/main/java/uk/me/ruthmills/exifinate/EXIFinateApplication.java
@@ -0,0 +1,12 @@
+package uk.me.ruthmills.exifinate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EXIFinateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EXIFinateApplication.class, args);
+	}
+}
